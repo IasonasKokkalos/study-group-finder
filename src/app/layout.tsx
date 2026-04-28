57b82980744df01422c6865a56417faaad1564f6
Jason <i.kokkalos@student.tue.nl>
2026-04-28 10:07:11 +0200

    Add home page redirect and clean up layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./global.css";
+
+export const metadata: Metadata = {
+    title: "Study Group Finder - TU/e",
+    description: "Find and create study sessions wtih fellow TU/e students",
+};
+
+export default function RootLayout ({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+   return (
+    <html lang="en">
+      <body className="antialiased">
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import { createClient } from "@/lib/supabase/server";
+import { create } from "domain";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+    const supabase = await createClient();
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    if (user) {
+        redirect("/dashboard");
+    } else {
+        redirect("/auth/login");
+    }
+}
